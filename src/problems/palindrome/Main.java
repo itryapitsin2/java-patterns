f2c38e26860604d6a4ce3c255c73d9c656d5c7ad
itryapitsin <iliya.tryapitsin@gmail.com>
2024-04-28 12:43:08 +0300

    chore(problems): add Palindrome problem solution

---
+++ src/problems/palindrome/Main.java
@@ -0,0 +1,15 @@
+package problems.palindrome;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        System.out.printf("Solution1: 121 is " + solution1.isPalindrome(121) + "\n");
+        System.out.printf("Solution1: -121 " + solution1.isPalindrome(-121) + "\n");
+        System.out.printf("Solution1: 10 " + solution1.isPalindrome(10) + "\n");
+
+        Solution2 solution2 = new Solution2();
+        System.out.printf("Solution2: 121 is " + solution2.isPalindrome(121) + "\n");
+        System.out.printf("Solution2: -121 " + solution2.isPalindrome(-121) + "\n");
+        System.out.printf("Solution2: 10 " + solution2.isPalindrome(10) + "\n");
+    }
+}
