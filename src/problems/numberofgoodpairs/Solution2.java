779d5e21765d83674ce263ad198e3b98ed6b250e
itryapitsin <iliya.tryapitsin@gmail.com>
2024-04-29 12:19:23 +0300

    chore(problems): add Number of Good Pairs problem solution

---
+++ src/problems/numberofgoodpairs/Solution2.java
@@ -0,0 +1,17 @@
+package problems.numberofgoodpairs;
+
+public class Solution2 {
+    public int numIdenticalPairs(int[] nums) {
+        return helper(nums, 0, 0);
+    }
+
+    public int helper(int[] nums, int i, int count) {
+        if (i == nums.length) {
+            return count;
+        }
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {count++;}
+        }
+        return helper(nums, ++i, count);
+    }
+}
