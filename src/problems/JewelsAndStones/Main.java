eee270ddd78722c9ea8ce2b406b54b66884c45c3
itryapitsin <iliya.tryapitsin@gmail.com>
2024-04-30 00:18:10 +0300

    chore(problems): add Jewels and Stones problem solution

---
+++ src/problems/JewelsAndStones/Main.java
@@ -0,0 +1,14 @@
+package problems.JewelsAndStones;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        System.out.printf("Solution1: jewels = \"aA\", stones = \"aAAbbbb\" is " + solution1.numJewelsInStones("aA", "aAAbbbb") + "\n");
+        System.out.printf("Solution1: jewels = \"z\", stones = \"ZZ\" is " + solution1.numJewelsInStones("z", "ZZ") + "\n");
+
+        Solution2 solution2 = new Solution2();
+        System.out.printf("Solution2: jewels = \"aA\", stones = \"aAAbbbb\" is " + solution2.numJewelsInStones("aA", "aAAbbbb") + "\n");
+        System.out.printf("Solution2: jewels = \"z\", stones = \"ZZ\" is " + solution2.numJewelsInStones("z", "ZZ") + "\n");
+    }
+}
---
+++ src/problems/JewelsAndStones/Solution2.java
@@ -0,0 +1,15 @@
+package problems.JewelsAndStones;
+
+public class Solution2 {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+
+        for (char c : stones.toCharArray()) {
+            if (jewels.indexOf(c) != -1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
